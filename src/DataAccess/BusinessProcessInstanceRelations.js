6665df32a70828ecb7da8fa93cc8b4cd711f3e6c
unknown <dlesertesseur@gmail.com>
2023-10-12 15:36:39 -0300

    v.0.0.1

---
+++ src/DataAccess/BusinessProcessInstanceRelations.js
@@ -0,0 +1,23 @@
+import { API } from "../Constants";
+
+async function findAllBusinessProcessInstanceRelationsById(parameters) {
+  try {
+    const requestOptions = {
+      method: "GET",
+      mode: "cors",
+      headers: { "Content-Type": "application/json", token: parameters.token },
+    };
+
+    const url = API.businessProcessInstanceRelation.findAllById + parameters.id;
+    const res = await fetch(url, requestOptions);
+    const data = await res.json();
+
+    return data;
+  } catch (error) {
+    return error;
+  }
+}
+
+export {
+  findAllBusinessProcessInstanceRelationsById
+};
